cbdd7bfa5ed342b88b7e4face79477bffd16ef3e
Sujay Poojari <sujaypoojari01@gmail.com>
2018-08-23 16:46:54 +0530

    serach filter

--- oshop/src/app/admin/admin-products/admin-products.component.ts
+++ oshop/src/app/admin/admin-products/admin-products.component.ts
@@ -1,30 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../../product.service';
-
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { ProductService } from "../../product.service";
+import { Subscription, Observable } from "rxjs";
+import { AngularFireDatabase } from "angularfire2/database";
 
 @Component({
-  selector: 'app-admin-products',
-  templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+  selector: "app-admin-products",
+  templateUrl: "./admin-products.component.html",
+  styleUrls: ["./admin-products.component.css"]
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
   products$;
+  products: Observable<any[]>;
+  filteredProducts: any[];
+  subscription: Subscription;
   dataRef;
-  constructor(private productService : ProductService) { 
+  constructor(
+    private productService: ProductService,
+    private db: AngularFireDatabase
+  ) {
     // this.dataRef=this.productService.getAll();
     // this.products$=this.dataRef.snapshotChanges().pipe(map(changes=>{
     //   console.log(changes);
-      
+
     // }));
 
+    /* this.subscription = this.productService
+      .getAll()
+      .valueChanges()
+      .subscribe(product => (this.filteredProducts = this.products = product));
+    console.log(this.products); */
+
     this.products$ = productService.getAll().valueChanges();
-    console.log(this.products$);
-    
+    this.products = db.list("/products").valueChanges();
+    console.log(this.products);
   }
 
-  ngOnInit() {
-  }
+  /* filter(query: string) {
+    this.filteredProducts = query
+      ? this.products.filter(p =>
+          p.title.toLowerCase().includes(query.toLocaleLowerCase())
+        )
+      : this.products;
+  } */
+  ngOnInit() {}
 
-
-
+  ngOnDestroy() {}
 }
